3de6e4b2b57c506b60b9b28664f6aaab2faaf6fb
Milvintsiss <milvintsiss@gmail.com>
2023-01-18 17:32:35 +0100

    Fixes bsp working only with some coordinates

--- ex03/srcs/bsp.cpp
+++ ex03/srcs/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: ple-stra <ple-stra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 16:28:05 by ple-stra          #+#    #+#             */
-/*   Updated: 2023/01/18 16:58:41 by ple-stra         ###   ########.fr       */
+/*   Updated: 2023/01/18 17:30:48 by ple-stra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,11 @@ bool bsp(Point const a, Point const b, Point const c, Point const point)
 			+ a.x() * (b.y() - c.y())
 			+ b.x() * c.y());
 
-	Fixed const s = Fixed(0.44f)
+	Fixed const s = (Fixed(1) / (Fixed(2) * signedArea))
 		* (a.y() * c.x() - a.x() * c.y()
 			+ (c.y() - a.y()) * point.x()
 			+ (a.x() - c.x()) * point.y());
-	Fixed const t = Fixed(0.44f)
+	Fixed const t = (Fixed(1) / (Fixed(2) * signedArea))
 		* (a.x() * b.y() - a.y() * b.x()
 			+ (a.y() - b.y()) * point.x()
 			+ (b.x() - a.x()) * point.y());
