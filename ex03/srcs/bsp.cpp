cebc7b6a2a7adb5e08d07cd50592473c301bb22e
Milvintsiss <milvintsiss@gmail.com>
2023-01-18 16:59:12 +0100

    Makes ouptut of ex03 more readable

--- ex03/srcs/bsp.cpp
+++ ex03/srcs/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: ple-stra <ple-stra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 16:28:05 by ple-stra          #+#    #+#             */
-/*   Updated: 2023/01/17 22:39:24 by ple-stra         ###   ########.fr       */
+/*   Updated: 2023/01/18 16:58:41 by ple-stra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,14 +29,9 @@ bool bsp(Point const a, Point const b, Point const c, Point const point)
 		* (a.x() * b.y() - a.y() * b.x()
 			+ (a.y() - b.y()) * point.x()
 			+ (b.x() - a.x()) * point.y());
-	
-	std::cout << (Fixed(1) / (Fixed(2) * signedArea)) << std::endl;
-	std::cout << (a.y() * c.x() - a.x() * c.y()) << std::endl;
-	std::cout << ((c.y() - a.y()) * point.x()) << std::endl;
-	std::cout << ((a.x() - c.x()) * point.y()) << std::endl;
 
-	std::cout << "Signed Area: " << signedArea << std::endl;
+	std::cout << std::endl << "Signed Area: " << signedArea << std::endl;
 	std::cout << "s: " << s << std::endl;
-	std::cout << "t: " << t << std::endl;
+	std::cout << "t: " << t << std::endl << std::endl;
 	return (s > Fixed(0) && t > Fixed(0) && Fixed(1) - s - t > Fixed(0));
 }
